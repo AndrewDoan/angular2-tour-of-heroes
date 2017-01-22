ca29e6af4b6803a299040cd4c922faccfbfcdb95
Andrew Doan <blooot@gmail.com>
2017-01-22 03:01:04 -0800

    through step 5

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `,
+})
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+}
