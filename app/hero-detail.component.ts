c3a9f21ae5a9cb613dd850c7c338187a978068a5
Andrew Doan <blooot@gmail.com>
2017-01-25 16:19:21 -0800

    finished http/tutorial

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,42 +1,32 @@
 import 'rxjs/add/operator/switchMap';
-
-// Keep the Input import for now, we'll remove it later:
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
-
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 import { Hero }        from './hero';
 import { HeroService } from './hero.service';
-
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: 'hero-detail.component.html',
-  styleUrls: [ 'hero-detail.component.css' ],
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css' ]
 })
-
 export class HeroDetailComponent implements OnInit {
-
-  @Input()
   hero: Hero;
-
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
-
   ngOnInit(): void {
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
       .subscribe(hero => this.hero = hero);
   }
-
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
   goBack(): void {
     this.location.back();
   }
-
- // Going back too far could take us out of the application. That's acceptable in a demo. We'd guard against it in a real application, perhaps with the CanDeactivate guard.
-
-
 }
